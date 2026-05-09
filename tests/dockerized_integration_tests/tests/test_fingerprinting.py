e62ae450463dba74a501ff240cd043a4ad996002
Lukáš Selický <lukas.selicky@gmail.com>
2026-05-09 17:42:21 +0200

    change JSS fingerprinting API test logic

--- tests/dockerized_integration_tests/tests/test_fingerprinting.py
+++ tests/dockerized_integration_tests/tests/test_fingerprinting.py
@@ -1,22 +1,18 @@
 import pytest
-import requests
-from config import WEB_CONTROL_URL, WEB_HTTP_PUBLIC_URL
+from config import WEB_HTTP_PUBLIC_URL
 from utils import make_driver, wait_result
 
 def run_fingerprinting_case(load_jshelter, browser, test_case):
-    requests.post(f"{WEB_CONTROL_URL}/fingerprinting-reset", timeout=2)
     driver = make_driver(load_jshelter, browser)
     try:
         driver.get(f"{WEB_HTTP_PUBLIC_URL}/fingerprinting?case={test_case}")
-        wait_result(driver)
-        logs = requests.get(f"{WEB_CONTROL_URL}/fingerprinting-logs", timeout=2).json()
-        payload = logs[-1]
-        return payload["payload"]["result"]
+        return wait_result(driver)
     finally:
         driver.quit()
 
-@pytest.mark.parametrize("browser", [ "firefox", "chrome"])
-@pytest.mark.parametrize("test_case", [ "timer", "canvas", "webgl", "hardwareConcurrency" ])
+
+@pytest.mark.parametrize("browser", ["firefox", "chrome"])
+@pytest.mark.parametrize("test_case", ["timer", "canvas", "webgl", "hardwareConcurrency"])
 def test_fingerprinting(test_case, browser):
     unchanged_result = run_fingerprinting_case(False, browser, test_case)
     protected_result = run_fingerprinting_case(True, browser, test_case)
